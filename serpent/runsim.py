67f9b979797395d2793c85c0cc3f94781c482521
Heiko Heiko <heiko@heiko.org>
2014-09-12 17:50:34 +0200

    increase block gas_limit

--- serpent/runsim.py
+++ serpent/runsim.py
@@ -1,16 +1,20 @@
 # execute this to run a battle
 from testenv import tester
 from testenv import logger
-tester.gas_limit = 10**5 - 10**4
+
+SIM_GAS = 10**6
 
 def run_simulation():
     s = tester.state()
+    s.block.gas_limit = SIM_GAS * 2
+    tester.gas_limit = SIM_GAS
     simulation = s.contract('simulation.se')
     logger.register_address('sim', simulation)
     a_ai = s.contract('ai_simple.se')
     logger.register_address('a_ai', a_ai)
     b_ai = s.contract('ai_simple.se')
     logger.register_address('b_ai', b_ai)
+
     winner = s.send(tester.k0, simulation, 0, data=[a_ai, b_ai])
 
     if not winner:
