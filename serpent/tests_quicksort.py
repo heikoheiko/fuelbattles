0b60c0f86620c849cc64830ec4d46934eca38ad3
Heiko Heiko <heiko@heiko.org>
2014-09-13 13:15:38 +0200

    minimal test

--- serpent/tests_quicksort.py
+++ serpent/tests_quicksort.py
@@ -3,15 +3,20 @@
 from testenv import tester
 from testenv import run, logger
 
-def test_quicksort():
-    s = tester.state()
-    c = s.contract('quicksort_pairs_debug.se')
-    logger.register_address('qsort', c)
-    data = [ 30, 1, 90, 2, 70, 3, 50, 4]
-    expected = [ 30, 1, 50, 4, 70, 3, 90, 2 ]
-    print 'sorting', data
+s = tester.state()
+c = s.contract('quicksort_pairs_debug.se')
+logger.register_address('qsort', c)
+
+def test_quicksort(data, expected):
+
+
+    print 'testing', data, expected
     r = s.send(tester.k0, c, 0, data=data)
     assert expected == r, (r, expected)
-test_quicksort()
+
+test_quicksort([30, 1],[30, 1])
+test_quicksort([30, 1, 20, 2],[20, 2, 30, 1])
+test_quicksort([30, 0, 20, 2],[20, 2, 30, 0])
+test_quicksort([30, 1, 90, 2, 70, 3, 50, 4],[ 30, 1, 50, 4, 70, 3, 90, 2 ])
 
 
